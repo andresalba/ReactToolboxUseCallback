50e20e3b137a703b1a843c1c6f32abcb70940112
Andres Alba <tensinkusum@example.com>
2022-09-20 14:31:39 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import React, { useCallback, useState } from 'react';
+import List from './List.js';
 import './App.css';
 
 function App() {
+  const [number, setNumber] = useState(1);
+  const [dark, setDark] = useState(false);
+
+  const getItems = useCallback(() => { //returns the entire function
+    return [number, number+1, number+2]
+  }, [number]) //changes only when number changes
+
+  const theme = {
+    backgroundColor: dark ? '#333' : '#FFF',
+    color: dark ? '#FFF': '#333'
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={theme}>
+      <input
+        type="number"
+        value={number}
+        onChange={e => setNumber(parseInt(e.target.value))}
+      />
+      <button onClick={() => setDark( prevDark => !prevDark )}>
+        Toggle theme
+      </button>
+      <List getItems={getItems}/>
     </div>
   );
 }
